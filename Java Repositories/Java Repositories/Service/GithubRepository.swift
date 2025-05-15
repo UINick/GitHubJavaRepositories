8158a6fcce3893b5d703a33a103916a7ce9b142d
UINick <nicholas_developer@outlook.com>
2025-05-14 23:50:42 -0300

    merge

--- Java Repositories/Java Repositories/Service/GithubRepository.swift
+++ Java Repositories/Java Repositories/Service/GithubRepository.swift
@@ -13,9 +13,6 @@ protocol GitHubRepoRepositoryProtocol {
 
 class GitHubRepoRepository: GitHubRepoRepositoryProtocol {
     func getPopularJavaRepositories(_ page: Int) async throws -> [JavaProjectsModel] {
-        print("CHAMEEEEEEEI")
-        print("+++++++++++++++++++++++")
-        print("+++++++++++++++++++++++ page \(page)")
         let url = URL(string: "https://api.github.com/search/repositories?q=language:Java&sort=stars&%20page=\(page)")!
         let (data, _) = try await URLSession.shared.data(from: url)
         let decoded = try JSONDecoder().decode(SearchResponse.self, from: data)
